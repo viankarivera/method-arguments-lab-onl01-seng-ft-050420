b440af7c35fc391ece0ebd057890f2b17e38d2a9
Vianka Rivera <viankalrivera@gmail.com>
2020-05-04 22:26:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,4 +5,5 @@ def introduction(name)
 end
 
 def introduction_with_language(language)
-  puts "Hi, my name is #{name} and I am "
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
